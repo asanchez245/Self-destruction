5dbf0cfdf1483d524c1db62174f2aed80b969099
asanchez245 <asanchezcuartero@gmail.com>
2025-01-30 11:42:23 +0100

    Basic Player Controller

---
+++ Self-destruction Project/Assets/PlayerController_Clase.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController_Clase : MonoBehaviour
+{
+    #region VARIABLES
+    [HideInInspector] public Vector3 playerPos;
+    [HideInInspector] public Vector2 movement;
+    [HideInInspector] public Rigidbody2D rb;
+
+    public bool playerInput;
+
+    [SerializeField] float _speed;
+
+    #endregion
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        playerInput = true;
+    }
+
+
+    void Update()
+    {
+
+        //Movimiento del Player
+        playerPos = transform.position;
+
+        float movementX = Input.GetAxisRaw("Horizontal");
+        float movementY = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(movementX, movementY).normalized;
+    }
+    private void FixedUpdate()
+    {
+        //C�lculo de f�sicas del moviemiento del Player
+        if (playerInput)
+        {
+            rb.linearVelocity = new Vector2(movement.x * _speed, movement.y * _speed);
+        }
+    }
+}
+
